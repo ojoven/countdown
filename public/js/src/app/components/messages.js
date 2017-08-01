123b92d514aed76fb6505e8a6ddb5357cfbae119
ojoven <mikeltorresmail@gmail.com>
2017-08-01 17:21:47 +0200

    Add message section

---
+++ public/js/src/app/components/messages.js
@@ -0,0 +1,24 @@
+Vue.component('messages', {
+
+	template: `
+		<div id="messages">
+			<p>There’s a nuclear bomb set by our enemy that will explode in a minute. A squad of brave citizens and warriors will need to fight together to prevent the bomb from firing.</p>
+			<p>How more will the humanity last?</p>
+		</div>
+  `,
+
+	data() {
+		return {
+
+		}
+	},
+
+	created: function() {
+
+	},
+
+	methods: {
+
+	}
+
+});
